bc9c147e74d22ebf53e3b562c55eb3ec547d4143
zm50 <2548604505@qq.com>
2025-01-12 10:07:26 +0800

    构建web服务

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,52 @@
+use std::{collections::HashMap, sync::OnceLock};
+
+use axum::{
+    extract::Query, routing::get, Json, Router
+};
 use ollama_rs::{
     generation::completion::request::GenerationRequest, Ollama
 };
 
+#[derive(Debug)]
+struct Context {
+    ollama: Ollama,
+}
+
+static CONTEXT: OnceLock<Context> = OnceLock::new();
+
+impl Context {
+    fn global() -> &'static Self {
+        CONTEXT.get_or_init(|| {
+            println!("Initializing Ollama...");
+            Context {
+                ollama: Ollama::default(),
+            }
+        })
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ollama = Ollama::default();
+async fn main() {
+    let app = Router::new().route("/", get(chat));
 
-    let req = GenerationRequest::new("qwen2.5:0.5b".into(), "Hello, world!".to_string());
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
 
-    let res = ollama.generate(req).await?;
-    println!("Response: {}", res.response);
-    Ok(())
+async fn chat(Query(mut query): Query<HashMap<String, String>>) -> Json<String> {
+    let prompt = if let Some(prompt) = query.remove("prompt") {
+        prompt
+    } else {
+        return Json("Error: prompt not provided".to_string());
+    };
+
+    let req = GenerationRequest::new("qwen2.5:0.5b".into(), prompt);
+    let res = Context::global().ollama.generate(req).await;
+    Json(
+        if let Ok(res) = res {
+            res.response
+        } else {
+            "Error".to_string()
+        }
+    )
 }
